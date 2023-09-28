3d5a6863c3f8fcfdab15d2a9ec7df9454414a9b4
MikhailVolvach <mihailvolvach@gmail.com>
2023-09-28 14:07:35 +0300

    Добавил статус заявки на карточку услуги

--- app/collections.py
+++ app/collections.py
@@ -3,9 +3,26 @@ from app.models.data_item import DataItem
 
 
 data_collection = [
-    DataItem("img/Heart_of_a_dog.jpg", "Булгаков М. А. Собачье сердце", Path(data_path, "Heart_of_a_dog.txt"), True, False),
-    DataItem("img/Directory.png", "Директория KTS", Path(data_path, "KTS"), False, True),
-    DataItem("img/Code.png", "Задача Tinkoff Start", Path(data_path, "Tinkoff_task.txt"), True, False, True),
-    DataItem("img/Master.jpg", "Булгаков М. А. Мастер и Маргарита", Path(data_path, "Master.txt"), True, False)
+    DataItem(
+        img="img/Heart_of_a_dog.jpg", 
+        title="Булгаков М. А. Собачье сердце", 
+        value=Path(data_path, "Heart_of_a_dog.txt"), 
+        is_file=True),
+    DataItem(
+        img="img/Directory.png", 
+        title="Директория KTS", 
+        value=Path(data_path, "KTS"), 
+        is_iterable=True),
+    DataItem(
+        img="img/Code.png", 
+        title="Задача Tinkoff Start", 
+        value=Path(data_path, "Tinkoff_task.txt"), 
+        is_file=True, 
+        is_code=True),
+    DataItem(
+        img="img/Master.jpg", 
+        title="Булгаков М. А. Мастер и Маргарита", 
+        value=Path(data_path, "Master.txt"), 
+        is_file=True)
 ]
 
